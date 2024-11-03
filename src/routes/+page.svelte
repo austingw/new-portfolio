32e8f6a6fbd43f8fb6d5d5529e96fd34ae063ed7
austingw <senortinman@gmail.com>
2024-11-03 15:14:39 -0500

    Basic header layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
+	import GitHub from '$lib/icons/gh.svelte';
 </script>
 
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-start w-full h-full px-8">
+	<h1 class="text-[15rem]">austingw</h1>
+	<div class="bg-cream w-full h-[1px]"></div>
+	<div class="flex flex-row items-between justify-between w-full">
+		<p>Austin Williams | Software Developer | austin@austingw.com</p>
+		<div>
+			<GitHub />
+			<!-- <LinkedIn /> -->
+		</div>
+	</div>
+</div>
