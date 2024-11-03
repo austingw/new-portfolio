e562145f8fe6be4f640ece916a956d2b9b1d7e70
austingw <senortinman@gmail.com>
2024-11-03 15:39:16 -0500

    Add all icons to page and add divs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
+	import Email from '$lib/icons/Email.svelte';
 	import GitHub from '$lib/icons/GitHub.svelte';
+	import LinkedIn from '$lib/icons/LinkedIn.svelte';
 </script>
 
-<div class="flex flex-col items-center justify-start w-full h-full px-8">
+<div class="flex flex-col items-center justify-start w-full h-full px-8 gap-4">
 	<h1 class="text-[15rem]">austingw</h1>
 	<div class="bg-cream w-full h-[1px]"></div>
-	<div class="flex flex-row items-between justify-between w-full">
-		<p>Austin Williams | Software Developer | austin@austingw.com</p>
-		<div>
+	<div class="flex flex-row items-center justify-between w-full">
+		<p>Austin Williams | Full-stack Software Developer</p>
+		<div class="flex flex-row items-center justify-end gap-2">
 			<GitHub />
-			<!-- <LinkedIn /> -->
+			<LinkedIn />
+			<Email />
 		</div>
 	</div>
+
+	<div class="bg-cream w-full h-[4px]"></div>
+	<div class="bg-cream w-full h-[16px]"></div>
+	<div class="bg-cream w-full h-[32px]"></div>
+	<div class="bg-cream w-full h-[64px]"></div>
+	<div class="bg-cream w-full h-[128px]"></div>
 </div>
