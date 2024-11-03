43248e21d421b338bf8608233a81b515d2fa9eec
austingw <senortinman@gmail.com>
2024-11-03 15:29:53 -0500

    Add IconWrapper component

---
+++ src/lib/icons/IconWrapper.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { children, url }: { children: Snippet; url: string } = $props();
+</script>
+
+<a href={url}>
+	<button class="hover:opacity-70 active:opacity-35">
+		{@render children()}
+	</button>
+</a>
