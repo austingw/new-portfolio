0b6327290ab264e5f4baa203c94972e0ecc9e723
austingw <senortinman@gmail.com>
2024-11-04 18:33:39 -0500

    Move header to seperate copmonent

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Bars from './Bars.svelte';
+	import Divider from './Divider.svelte';
+	import Email from '../icons/Email.svelte';
+	import GitHub from '../icons/GitHub.svelte';
+	import LinkedIn from '../icons/LinkedIn.svelte';
+</script>
+
+<div class={`flex flex-col items-center justify-start w-full h-full gap-4 animate-fade`}>
+	<h1 class="text-[19.5vw] stretch-wide">austingw</h1>
+	<Divider />
+	<div class="flex flex-col sm:flex-row items-end sm:items-center justify-between w-full gap-2">
+		<p class="font-light stretch-light hidden sm:block">Austin Williams | Software Developer</p>
+		<p class="font-light stretch-light sm:hidden">Austin Williams</p>
+		<p class="font-light stretch-light sm:hidden">Software Developer</p>
+		<div class="flex flex-row items-center justify-end gap-2">
+			<GitHub />
+			<LinkedIn />
+			<Email />
+		</div>
+	</div>
+	<Bars />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
-	import Bars from '$lib/components/Bars.svelte';
-	import Email from '$lib/icons/Email.svelte';
-	import GitHub from '$lib/icons/GitHub.svelte';
-	import LinkedIn from '$lib/icons/LinkedIn.svelte';
+	import About from '$lib/components/About.svelte';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
 <div class={`flex flex-col items-center justify-start w-full h-full px-8 gap-4 animate-fade`}>
-	<h1 class="text-[19.5vw] stretch-wide">austingw</h1>
-	<div class="bg-cream w-full h-[1px]"></div>
-	<div class="flex flex-col sm:flex-row items-start sm:items-center justify-between w-full">
-		<p class="font-light stretch-light">Austin Williams | Full-stack Software Developer</p>
-		<div class="flex flex-row items-center justify-end gap-2">
-			<GitHub />
-			<LinkedIn />
-			<Email />
-		</div>
-	</div>
-	<Bars />
-	<Bars reverse />
+	<Header />
+	<About />
 </div>
