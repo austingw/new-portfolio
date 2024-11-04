564e5d226cc295cba1574d834106aab4f71bdaa1
austingw <senortinman@gmail.com>
2024-11-03 20:43:45 -0500

    Make bars component

---
+++ src/lib/components/Bars.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { reverse }: { reverse?: boolean } = $props();
+</script>
+
+<div
+	class={`flex ${reverse ? 'flex-col-reverse' : 'flex-col'} w-full items-center justify-center gap-4 `}
+>
+	<div class="bg-cream w-full h-[4px]"></div>
+	<div class="bg-cream w-full h-[16px]"></div>
+	<div class="bg-cream w-full h-[32px]"></div>
+	<div class="bg-cream w-full h-[64px]"></div>
+	<div class="bg-cream w-full h-[128px]"></div>
+</div>
