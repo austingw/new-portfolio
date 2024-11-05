e54618cc0ce3eb52a98e0d1a4d935f9e6f5dac09
austingw <senortinman@gmail.com>
2024-11-05 18:18:16 -0500

    Add header for contact section

---
+++ src/lib/components/Contact.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Divider from './Divider.svelte';
+</script>
+
+<div class={`flex flex-col items-start justify-start w-full h-full gap-4 animate-fade`}>
+	<Divider height={1} />
+	<h2 class="text-[2em] stretch-wide">Contact</h2>
+	<Divider height={4} />
+</div>
