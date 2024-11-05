561f92af634dcc17e8bcd23e8a2b32371989dc4e
austingw <senortinman@gmail.com>
2024-11-04 19:09:08 -0500

    Setup project component

---
+++ src/lib/components/ProjectBlock.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import type { Project } from '../types';
+	let { project }: { project: Project } = $props();
+</script>
+
+<div></div>
---
+++ src/lib/components/Projects.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import ProjectBlock from './ProjectBlock.svelte';
+</script>
+
+<div class={`flex flex-row items-start justify-start w-full h-full gap-4 animate-fade`}></div>
