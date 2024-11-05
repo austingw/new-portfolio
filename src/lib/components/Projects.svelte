b1b970ff485d6f43447628e7f3bb23fbdf33041d
austingw <senortinman@gmail.com>
2024-11-05 18:24:43 -0500

    Render projects based on list

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
 	import Divider from './Divider.svelte';
 	import ProjectBlock from './ProjectBlock.svelte';
+	import projects from '../projects';
 </script>
 
 <div class={`flex flex-col items-start justify-start w-full h-full gap-4 animate-fade`}>
 	<Divider height={1} />
 	<h2 class="text-[2em] stretch-wide">About me</h2>
 	<Divider height={4} />
-	<ProjectBlock
-		project={{
-			title: 'test',
-			description: 'Lorem ipsum whatever whatever',
-			techStack: ['svelte', 'go'],
-			url: '',
-			repo: '',
-			imageSrc: ''
-		}}
-		index={0}
-	/>
+	{#each projects as project, index}
+		<ProjectBlock {project} {index} />
+	{/each}
 </div>
