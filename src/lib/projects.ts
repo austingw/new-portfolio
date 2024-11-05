ead1f52c95086e6ca7b772bcce237c4ed3fa4c76
austingw <senortinman@gmail.com>
2024-11-05 18:24:28 -0500

    Move project list out to a seperate file

---
+++ src/lib/projects.ts
@@ -0,0 +1,14 @@
+import type { Project } from './types';
+
+const projects: Project[] = [
+	{
+		title: 'test',
+		description: 'Lorem ipsum whatever whatever',
+		techStack: ['svelte', 'go'],
+		url: '',
+		repo: '',
+		imageSrc: ''
+	}
+];
+
+export default projects;
