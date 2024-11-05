e5a70f63a8bef0c496e495e5353b3ae3a653db13
austingw <senortinman@gmail.com>
2024-11-05 18:03:16 -0500

    Fix styling for project blocks

--- src/lib/components/ProjectBlock.svelte
+++ src/lib/components/ProjectBlock.svelte
@@ -4,29 +4,35 @@
 	let { project, index }: { project: Project; index: number } = $props();
 </script>
 
-<div class="flex flex-col items-start justify-start">
-	<h2 class="vert-text text-[3em] font-bold stretch-wide">{`0${index + 1}.`}</h2>
-	<div>
-		<p class="text-[2em] stretch-light">{project.title}</p>
+<div class="flex flex-col items-start justify-start p-4">
+	<h2 class="vert-text text-[3em] font-bold stretch-wide pb-2">{`0${index + 1}.`}</h2>
+	<div class="flex flex-col gap-2">
+		<p class="font-bold stretch-wide">{project.title}</p>
 		<p class="stretch-light">{project.description}</p>
-		<p class="stretch-thin italic">tech stack: {project.techStack.join(', ')}</p>
+		<p class="stretch-light">tech stack: {project.techStack.join(', ')}</p>
 		<div class="flex flex-row items-center justify-start gap-2">
-			<a
-				href={project.url}
-				target="_blank"
-				rel="noopener noreferrer"
-				class="text-sblue flex flex-row items-start justify-center gap-1 hover:opacity-70 active:opacity-35"
-			>
-				View <Arrow url={project.url} />
-			</a>
-			<a
-				href={project.repo}
-				target="_blank"
-				rel="noopener noreferrer"
-				class="text-sblue flex flex-row items-start justify-center gap-1 hover:opacity-70 active:opacity-35"
-			>
-				Repo <Arrow url={project.repo} />
-			</a>
+			<div class="flex flex-row items-start justify-center gap-1 text-sblue">
+				<a
+					href={project.url}
+					target="_blank"
+					rel="noopener noreferrer"
+					class="hover:opacity-70 active:opacity-35"
+				>
+					View
+				</a>
+				<Arrow url={project.url} />
+			</div>
+			<div class="flex flex-row items-start justify-center gap-1 text-sblue">
+				<a
+					href={project.url}
+					target="_blank"
+					rel="noopener noreferrer"
+					class="hover:opacity-70 active:opacity-35"
+				>
+					Repo
+				</a>
+				<Arrow url={project.repo} />
+			</div>
 		</div>
 	</div>
 </div>
