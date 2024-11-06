f8f8339fa9b294aea32106be95bf883abdddee3f
austingw <senortinman@gmail.com>
2024-11-05 21:47:20 -0500

    Conditional rendering for links

--- src/lib/components/ProjectBlock.svelte
+++ src/lib/components/ProjectBlock.svelte
@@ -5,34 +5,43 @@
 </script>
 
 <div class="flex flex-col items-start justify-start p-4">
-	<h2 class="vert-text text-[3em] font-bold stretch-wide pb-2">{`0${index + 1}.`}</h2>
+	<h2 class="vert-text text-[6em] font-bold stretch-wide pb-8">
+		{`0${index + 1}.`}
+	</h2>
 	<div class="flex flex-col gap-2">
 		<p class="font-bold stretch-wide">{project.title}</p>
 		<p class="stretch-light">{project.description}</p>
-		<p class="stretch-light">tech stack: {project.techStack.join(', ')}</p>
+		<p class="text-sm stretch-light">Made with: {project.techStack.join(', ')}</p>
 		<div class="flex flex-row items-center justify-start gap-2">
-			<div class="flex flex-row items-start justify-center gap-1 text-sblue">
-				<a
-					href={project.url}
-					target="_blank"
-					rel="noopener noreferrer"
-					class="hover:opacity-70 active:opacity-35"
-				>
-					View
-				</a>
-				<Arrow url={project.url} />
-			</div>
-			<div class="flex flex-row items-start justify-center gap-1 text-sblue">
-				<a
-					href={project.url}
-					target="_blank"
-					rel="noopener noreferrer"
-					class="hover:opacity-70 active:opacity-35"
-				>
-					Repo
-				</a>
-				<Arrow url={project.repo} />
-			</div>
+			{#if project.url.length > 0}
+				<div class="flex flex-row items-start justify-center gap-1 text-sblue">
+					<a
+						href={project.url}
+						target="_blank"
+						rel="noopener noreferrer"
+						class="hover:opacity-70 active:opacity-35"
+					>
+						View
+					</a>
+					<Arrow url={project.url} />
+				</div>
+			{/if}
+			{#if project.repo.length > 0}
+				<div class="flex flex-row items-start justify-center gap-1 text-sblue">
+					<a
+						href={project.url}
+						target="_blank"
+						rel="noopener noreferrer"
+						class="hover:opacity-70 active:opacity-35"
+					>
+						Repo
+					</a>
+					<Arrow url={project.repo} />
+				</div>
+			{/if}
+			{#if project.repo.length === 0 && project.url.length === 0}
+				<p class="text-sblue">WIP - Link Available Soon</p>
+			{/if}
 		</div>
 	</div>
 </div>
