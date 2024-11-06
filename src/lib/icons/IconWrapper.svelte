f818908fb55ffbfaa5819464882e1d2ecf52fc14
austingw <senortinman@gmail.com>
2024-11-05 22:23:24 -0500

    Make icons work like project links

--- src/lib/icons/IconWrapper.svelte
+++ src/lib/icons/IconWrapper.svelte
@@ -4,7 +4,7 @@
 	let { children, url }: { children: Snippet; url: string } = $props();
 </script>
 
-<a href={url}>
+<a href={url} target="_blank" rel="noopener noreferrer">
 	<button class="hover:opacity-70 active:opacity-35">
 		{@render children()}
 	</button>
