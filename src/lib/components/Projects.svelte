62f4e84b9f79beb17e6ff654693b0283647a5459
austingw <senortinman@gmail.com>
2024-11-05 21:02:45 -0500

    Update section header

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -6,7 +6,7 @@
 
 <div class={`flex flex-col items-start justify-start w-full h-full gap-4 animate-fade`}>
 	<Divider height={1} />
-	<h2 class="text-[2em] stretch-wide">About me</h2>
+	<h2 class="text-[2em] stretch-wide">Projects</h2>
 	<Divider height={4} />
 	{#each projects as project, index}
 		<ProjectBlock {project} {index} />
