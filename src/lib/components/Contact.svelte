545da9b8315f787989f0480e4ac7fd5596e1aee3
austingw <senortinman@gmail.com>
2024-11-06 23:15:04 -0500

    Add req function

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
 	import Divider from './Divider.svelte';
+
+	let contact = $state({ name: '', email: '', message: '' });
+
+	async function sendEmail() {
+		const response = await fetch('/api', {
+			method: 'POST',
+			body: JSON.stringify(contact),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		const res = await response.json();
+		console.log(res);
+	}
 </script>
 
 <div class={`flex flex-col items-start justify-start w-full h-full gap-4 animate-fade`}>
@@ -9,21 +24,27 @@
 	<p>Please feel free to contact me via the form below.</p>
 	<div class="flex flex-col items-start justify-center w-full gap-4">
 		<input
+			bind:value={contact.name}
 			type="text"
 			placeholder="Name"
 			class="w-full p-2 bg-bgpurp border-2 border-cream placeholder:text-cream placeholder:opacity-50 focus:outline-none"
 		/>
 		<input
+			bind:value={contact.email}
 			type="email"
 			placeholder="Email"
 			class="w-full p-2 bg-bgpurp border-2 border-cream placeholder:text-cream placeholder:opacity-50 focus:outline-none"
 		/>
 		<textarea
+			bind:value={contact.message}
 			placeholder="Message"
 			class="w-full p-2 bg-bgpurp border-2 border-cream placeholder:text-cream placeholder:opacity-50 focus:outline-none"
 		></textarea>
 		<div class="flex flex-row items-center justify-end w-full">
-			<button class="border-2 border-cream py-2 px-8 hover:opacity-70 active:opacity-35">
+			<button
+				onclick={sendEmail}
+				class="border-2 border-cream py-2 px-8 hover:opacity-70 active:opacity-35"
+			>
 				Send
 			</button>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,23 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import Projects from '$lib/components/Projects.svelte';
+
+	async function test() {
+		const response = await fetch('/api', {
+			method: 'POST',
+			body: JSON.stringify({ test: 'test' }),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		const res = await response.json();
+		console.log(res);
+	}
 </script>
 
 <div class={`flex flex-col items-center justify-start w-full h-full px-8 gap-4 animate-fade`}>
+	<button onclick={test}>Test</button>
 	<Header />
 	<About />
 	<Projects />
