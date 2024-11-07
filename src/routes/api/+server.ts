3ed9359f9f9ad44facb867752ef8615109b7290e
austingw <senortinman@gmail.com>
2024-11-06 23:50:48 -0500

    Add sendEmail to POST method

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,8 +1,10 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import sendEmail from '$lib/server/sendEmail';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const data = await request.json();
 	console.log(data);
+	await sendEmail({ name: 'test', email: 'scaremony@gmail.com', message: 'test' });
 	return json({ message: 'success' });
 };
