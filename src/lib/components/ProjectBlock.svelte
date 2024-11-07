36f7a2fad3116d04fefc6bb282467ff86cd113b6
austingw <senortinman@gmail.com>
2024-11-06 22:30:19 -0500

    Finalize styling for project blox

--- src/lib/components/ProjectBlock.svelte
+++ src/lib/components/ProjectBlock.svelte
@@ -4,14 +4,16 @@
 	let { project, index }: { project: Project; index: number } = $props();
 </script>
 
-<div class="flex flex-col items-start justify-start p-4">
+<div class="flex flex-col items-start justify-start p-4 h-full">
 	<h2 class="vert-text text-[6em] font-bold stretch-wide pb-8">
 		{`0${index + 1}.`}
 	</h2>
-	<div class="flex flex-col gap-2">
-		<p class="font-bold stretch-wide">{project.title}</p>
-		<p class="stretch-light">{project.description}</p>
-		<p class="text-sm stretch-light">Made with: {project.techStack.join(', ')}</p>
+	<div class="flex flex-col items-start justify-between h-full gap-2">
+		<div class="flex flex-col items-start pt-4 gap-2">
+			<p class="font-bold stretch-wide">{project.title}</p>
+			<p class="stretch-light">{project.description}</p>
+			<p class="text-sm stretch-thin font-semibold">Made with: {project.techStack.join(', ')}</p>
+		</div>
 		<div class="flex flex-row items-center justify-start gap-2">
 			{#if project.url.length > 0}
 				<div class="flex flex-row items-start justify-center gap-1 text-sblue">
