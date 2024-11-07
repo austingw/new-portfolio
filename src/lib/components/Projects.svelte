de5c38c8d6a515883460837700d165f538f9289a
austingw <senortinman@gmail.com>
2024-11-06 22:30:07 -0500

    Add responsive layout for project list

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -8,7 +8,11 @@
 	<Divider height={1} />
 	<h2 class="text-[2em] stretch-wide">Projects</h2>
 	<Divider height={4} />
-	{#each projects as project, index}
-		<ProjectBlock {project} {index} />
-	{/each}
+	<div
+		class={'flex flex-col md:grid md:grid-cols-4 md:grid-rows-1 items-start justify-start w-full h-full gap-4'}
+	>
+		{#each projects as project, index}
+			<ProjectBlock {project} {index} />
+		{/each}
+	</div>
 </div>
