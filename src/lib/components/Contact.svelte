04bd966c0c47d6293b3b14e9a714fad50421b637
austingw <senortinman@gmail.com>
2024-11-07 20:35:00 -0500

    Add validation/notification

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,19 +1,49 @@
 <script lang="ts">
+	import { z } from 'zod';
 	import Divider from './Divider.svelte';
 
+	const emailSchema = z.object({
+		name: z.string().min(1).max(255),
+		email: z.string().email(),
+		message: z.string().min(2)
+	});
+
 	let contact = $state({ name: '', email: '', message: '' });
+	let notifications: string[] = $state([]);
+	let showNotification = $state(false);
 
 	async function sendEmail() {
-		const response = await fetch('/api', {
-			method: 'POST',
-			body: JSON.stringify(contact),
-			headers: {
-				'content-type': 'application/json'
-			}
-		});
+		const parser = emailSchema.safeParse(contact);
+		if (!parser.success) {
+			const errors = parser.error.issues.map((issue) => {
+				return `${String(issue.path[0]).toUpperCase()} Error: ${issue.message}`;
+			});
+			notifications = errors;
+			showNotification = true;
+			setTimeout(() => {
+				showNotification = false;
+			}, 5000);
+		}
 
-		const res = await response.json();
-		console.log(res);
+		// if (!validate.success) {
+		// 	notifications = validate.error.errors;
+		// 	showNotification = true;
+		// 	setTimeout(() => {
+		// 		showNotification = false;
+		// 	}, 5000);
+		// 	return;
+		// }
+		//
+		// const response = await fetch('/api', {
+		// 	method: 'POST',
+		// 	body: JSON.stringify(contact),
+		// 	headers: {
+		// 		'content-type': 'application/json'
+		// 	}
+		// });
+		//
+		// const res = await response.json();
+		// console.log(res);
 	}
 </script>
 
@@ -22,7 +52,7 @@
 	<h2 class="text-[2em] stretch-wide">Contact</h2>
 	<Divider height={4} />
 	<p>Please feel free to contact me via the form below.</p>
-	<div class="flex flex-col items-start justify-center w-full gap-4">
+	<div class="relative flex flex-col items-start justify-center w-full gap-4">
 		<input
 			bind:value={contact.name}
 			type="text"
@@ -48,5 +78,14 @@
 				Send
 			</button>
 		</div>
+		{#if showNotification}
+			<div
+				class="border-2 bg-cream text-bgpurp border-bgpurp w-fit h-fit absolute bottom-0 right-0 p-2 animate-fade"
+			>
+				{#each notifications as n}
+					<p>{n}</p>
+				{/each}
+			</div>
+		{/if}
 	</div>
 </div>
