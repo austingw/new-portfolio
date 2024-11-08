2d6b63735a34d567b5a74d6a299091c16f29d666
austingw <senortinman@gmail.com>
2024-11-07 20:43:39 -0500

    Finish endpoint

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,10 +1,10 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import type { Email } from '$lib/types';
 import sendEmail from '$lib/server/sendEmail';
 
 export const POST: RequestHandler = async ({ request }) => {
-	const data = await request.json();
-	console.log(data);
-	await sendEmail({ name: 'test', email: 'scaremony@gmail.com', message: 'test' });
-	return json({ message: 'success' });
+	const data: Email = await request.json();
+	const success = await sendEmail(data);
+	return json({ success });
 };
